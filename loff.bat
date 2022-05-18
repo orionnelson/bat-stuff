33f7ffc5d390c11d940036aa2713f35499f01393
Orion Nelson <44347765+orionnelson@users.noreply.github.com>
2022-05-18 16:32:10 -0400

    Update loff.bat

--- loff.bat
+++ loff.bat
@@ -1,2 +1,3 @@
 @echo off
-start cmd /k "taskkill /F /IM SignalRgb.exe && liquidctl --match kraken set sync color off"
+start "" "signalrgb://effect/apply/Good Night!"&& timeout /t 3 && start cmd /k "taskkill /F /IM SignalRgb.exe && liquidctl --match kraken set sync color off"
+ 
